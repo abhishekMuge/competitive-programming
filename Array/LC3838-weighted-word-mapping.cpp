aef3808e30052dab488a62cd164a797c063f96ca
abhishekmuge <muge111hhh@gmail.com>
2026-06-13 11:29:46 +0530

    solved daily problem

---
+++ Array/LC3838-weighted-word-mapping.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+        ans.reserve(words.size());
+        for(string word: words) {
+            int s = 0;
+            for(char c : word) {
+                s += weights[c - 'a'];
+            }
+            ans += 'z' - s % 26;
+        }
+        return ans; 
+    }
+};
