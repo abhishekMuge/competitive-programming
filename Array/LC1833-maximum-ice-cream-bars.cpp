b4cb8b5732041e9b03c1572a68249f7f581be6ed
abhishekmuge <muge111hhh@gmail.com>
2026-06-21 11:49:41 +0530

    Solved daily problem

---
+++ Array/LC1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        vector<int> freq(100001);
+
+        for(int cost: costs) {
+            freq[cost]++;
+        }
+        int ans = 0;
+
+        for(int price = 1; price <= 100000; price++) {
+            if(freq[price] == 0) continue;
+
+            int canBuy = min(freq[price], coins / price);
+
+            ans += canBuy;
+            coins -= canBuy * price;
+
+            if(coins < price) break;
+        }
+        return ans;
+    }
+};
